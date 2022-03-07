f64be4de3ae5f1b5c047e04abcc00b8c96c87c78
George Everitt <geveritt@appliedrelevance.com>
2022-03-07 12:14:58 -0500

    Updated server.js to require 'dotenv'

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const { SystemUpdate } = require('@material-ui/icons');
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+require('dotenv').config();
+
 
 const app = express();
 const port = process.env.PORT || 5000;
